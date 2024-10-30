dcef25c9baa93e68f2f1a7b4b44eb31f4b78310c
senumiminodya <senumiminodya456@gmail.com>
2024-10-30 11:31:49 +0530

    initiate project

---
+++ src/main/java/lk/ijse/cropmanagementsystem/CropManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cropmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/cropmanagementsystem/CropManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cropmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
